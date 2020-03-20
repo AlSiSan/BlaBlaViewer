b88825c123fc06bb765234698b262fb09b1b807b
elssbbboy <elssbbboy@gmail.com>
2020-03-20 22:10:10 +0100

    Revert "Revert "Added graphics""
    This reverts commit 8aa3efd778c20e067dee0c3b016a85736ce2f47c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,12 @@ import { MapViewerComponent } from './components/map-viewer/map-viewer.component
 import { AboutComponent } from './components/about/about.component';
 import { MapSidebarComponent } from './components/map-viewer/map-sidebar/map-sidebar.component';
 import { MapBottombarComponent } from './components/map-viewer/map-bottombar/map-bottombar.component';
-import { MapRsidebarComponent } from './components/map-viewer/map-rsidebar/map-rsidebar.component';
 import { LayersManagerComponent } from './components/map-viewer/layers-manager/layers-manager.component';
 import { SearchComponent } from './components/map-viewer/search/search/search.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DataOptionsComponent } from './components/map-viewer/data-options/data-options.component';
 import { FormsModule } from '@angular/forms';
+import { GraphicsModalComponent } from './components/map-viewer/graphics-modal/graphics-modal.component';
 
 @NgModule({
   declarations: [
@@ -25,10 +25,10 @@ import { FormsModule } from '@angular/forms';
     AboutComponent,
     MapSidebarComponent,
     MapBottombarComponent,
-    MapRsidebarComponent,
     LayersManagerComponent,
     SearchComponent,
-    DataOptionsComponent
+    DataOptionsComponent,
+    GraphicsModalComponent
   ],
   imports: [
     BrowserModule,
