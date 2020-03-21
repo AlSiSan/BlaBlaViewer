804b783a4584bfc5649eb86ec814bf9966c3fd77
elssbbboy <elssbbboy@gmail.com>
2020-03-21 22:47:02 +0100

    Added two charts, journeys per dest and ori

--- src/app/components/map-viewer/graphics-modal/graphics-modal.component.ts
+++ src/app/components/map-viewer/graphics-modal/graphics-modal.component.ts
@@ -14,6 +14,7 @@ export class GraphicsModalComponent implements OnInit {
 
   loadingGraphics: boolean;
   journeysPerDayChart: any;
+  journeysPerOriginChart: any;
 
   constructor( private comm: CommunicationService ) { }
 
@@ -22,6 +23,8 @@ export class GraphicsModalComponent implements OnInit {
     new Promise(r => setTimeout(r, 300)).then(() => {
       this.loadingGraphics = false;
       this.journeysPerDay();
+      this.journeysPerOrigin();
+      this.journeysPerDestination();
     });
   }
 
@@ -30,10 +33,10 @@ export class GraphicsModalComponent implements OnInit {
   }
 
   journeysPerDay() {
-    let journeysPerDayDf = this.comm.journeysDf.groupBy('DIA')
+    const journeysPerDayDf = this.comm.journeysDf.groupBy('DIA')
           .aggregate(group => group.count()).rename('aggregation', 'groupCount');
 
-    let serie = [];
+    const serie = [];
     const daysLabels = journeysPerDayDf.toArray('DIA');
 
     journeysPerDayDf.toArray('groupCount').forEach((journeysOneDay, index) => {
@@ -76,6 +79,110 @@ export class GraphicsModalComponent implements OnInit {
           },
           axisY: {
             axisTitle: 'Viajes',
+            axisClass: 'ct-axis-title',
+            offset: {
+              x: 0,
+              y: 0
+            },
+            textAnchor: 'middle',
+            flipTitle: false
+          }
+        }),
+          Chartist.plugins.tooltip()
+      ]
+    });
+  }
+
+  journeysPerOrigin() {
+    const journeysPerDayDf = this.comm.journeysDf.groupBy('ORIGEN_P')
+          .aggregate(group => group.count()).rename('aggregation', 'groupCount')
+          .filter(row => row.get('ORIGEN_P') !== 'Otros')
+          .sortBy('groupCount', true);
+
+    console.log(journeysPerDayDf);
+
+    this.journeysPerOriginChart = new Chartist.Bar('#journeysPerOriginChart', {
+      labels: journeysPerDayDf.head(10).toArray('ORIGEN_P'),
+      series: [journeysPerDayDf.head(10).toArray('groupCount')
+      ]
+    }, {
+      chartPadding: {
+        top: 10,
+        right: 35,
+        bottom: 30,
+        left: 30,
+      },
+      reverseData: true,
+      horizontalBars: true,
+      axisY: {
+        offset: 70,
+        labelInterpolationFnc: (value) => value.split('/')[0]
+      },
+      plugins: [
+        Chartist.plugins.ctAxisTitle({
+          axisX: {
+            axisTitle: 'Viajes',
+            axisClass: 'ct-axis-title',
+            offset: {
+              x: 0,
+              y: 40
+            },
+            textAnchor: 'middle'
+          },
+          axisY: {
+            axisTitle: 'Ciudades',
+            axisClass: 'ct-axis-title',
+            offset: {
+              x: 0,
+              y: 0
+            },
+            textAnchor: 'middle',
+            flipTitle: false
+          }
+        }),
+          Chartist.plugins.tooltip()
+      ]
+    });
+  }
+
+  journeysPerDestination() {
+    const journeysPerDayDf = this.comm.journeysDf.groupBy('DESTINO_P')
+          .aggregate(group => group.count()).rename('aggregation', 'groupCount')
+          .filter(row => row.get('DESTINO_P') !== 'Otros')
+          .sortBy('groupCount', true);
+
+    console.log(journeysPerDayDf);
+
+    this.journeysPerOriginChart = new Chartist.Bar('#journeysPerDestinationChart', {
+      labels: journeysPerDayDf.head(10).toArray('DESTINO_P'),
+      series: [journeysPerDayDf.head(10).toArray('groupCount')
+      ]
+    }, {
+      chartPadding: {
+        top: 10,
+        right: 35,
+        bottom: 30,
+        left: 30,
+      },
+      reverseData: true,
+      horizontalBars: true,
+      axisY: {
+        offset: 70,
+        labelInterpolationFnc: (value) => value.split('/')[0]
+      },
+      plugins: [
+        Chartist.plugins.ctAxisTitle({
+          axisX: {
+            axisTitle: 'Viajes',
+            axisClass: 'ct-axis-title',
+            offset: {
+              x: -10,
+              y: 40
+            },
+            textAnchor: 'middle'
+          },
+          axisY: {
+            axisTitle: 'Ciudades',
             axisClass: 'ct-axis-title',
             offset: {
               x: 0,
